596b16bcca23d52583a16362d0d6c82a765879b8
nachohotz <juanhotz611@gmail.com>
2022-09-15 15:08:31 -0300

    TASK: change router to mainRouter

--- api/src/app.ts
+++ api/src/app.ts
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 import errorMiddleware from './middlewares/error.middleware';
 import { corsOptions, envConfig } from './config/';
 
-import routes from './routes/index.route';
+import mainRouter from './routes/index.routes';
 
 const app: Application = express();
 const { NODE_ENV } = envConfig;
@@ -21,7 +21,7 @@ if (NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use('/', routes);
+app.use('/', mainRouter);
 app.use(errorMiddleware);
 
 export default app;
