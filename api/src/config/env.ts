2a8fe1de8856834accaf447f80337d2ad9fd4548
nachohotz <juanhotz611@gmail.com>
2022-09-15 14:34:56 -0300

    FIX: remove default assignment in envs

--- api/src/config/env.ts
+++ api/src/config/env.ts
@@ -3,8 +3,8 @@ dotenv.config();
 
 // TODO: Add DB envs here
 export const envConfig = {
-  API_PORT: process.env.API_PORT || 3001,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  API_PORT: process.env.API_PORT,
+  NODE_ENV: process.env.NODE_ENV,
   CLIENT_DEV_URL: process.env.CLIENT_DEV_URL,
   CLIENT_PROD_URL: process.env.CLIENT_PROD_URL,
 };
