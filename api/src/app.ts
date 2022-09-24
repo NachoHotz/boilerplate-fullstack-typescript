b7dce1904a24761b4527aa8dd716a28630700ad7
nachohotz <juanhotz611@gmail.com>
2022-09-24 12:47:09 -0300

    FIX: move helmet initializer at app.use top

--- api/src/app.ts
+++ api/src/app.ts
@@ -10,11 +10,11 @@ import mainRouter from './routes/index.routes';
 const app: Application = express();
 const { NODE_ENV } = envConfig;
 
+app.use(helmet());
 app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 app.use(express.json({ limit: '25mb' }));
 app.use(cookieParser());
 app.use(cors(corsOptions));
-app.use(helmet());
 
 if (NODE_ENV === 'development') {
   const morgan = require('morgan');
