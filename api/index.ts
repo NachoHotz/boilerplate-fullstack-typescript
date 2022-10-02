0a4da8a93babf869dba6f0ae20007ece5d83abed
nachohotz <juanhotz611@gmail.com>
2022-10-02 15:46:31 -0300

    FIX: root route handlers. Add winston logger

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,8 @@
 import app from './src/app';
-import { envConfig } from './src/config';
+import { envConfig, logger } from './src/config';
 
 const { API_PORT, NODE_ENV } = envConfig;
 
 app.listen(API_PORT || 3001, () => {
-  console.log(`Server running on PORT ${API_PORT || 3001} in ${NODE_ENV} mode`);
+  logger.info(`Server running on PORT ${API_PORT || 3001} in ${NODE_ENV} mode`);
 });
--- api/src/routes/index.routes.ts
+++ api/src/routes/index.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
-import * as controller from '../controllers/basicController.controller';
+import * as controller from '../controllers/rootController.controller';
 import NotFoundException from '../exceptions/NotFoundException';
 
 const mainRouter = Router();
 
-mainRouter.get('/', controller.basicController);
+mainRouter.get('/', controller.rootController);
 
 mainRouter.use('*', (_req, _res, next) =>
   next(new NotFoundException('This page does not exist')),
